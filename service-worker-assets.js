450594d31593ea641295fdef5c2aed67d48a4426
Nazar62 <624rsr@gmail.com>
2025-06-17 15:49:32 +0300

    g

--- service-worker-assets.js
+++ service-worker-assets.js
@@ -579,6 +579,14 @@
     {
       "hash": "sha256-o1qrUazFD8MPygFt+oWr3C+bSJuECWGcOxNQzps7NEk=",
       "url": "brotliloader.min.js"
+    },
+    {
+      "hash": "sha256-wX1LB4yuBRFM4fmdSsH6jBlCch9uCHXV9uFTxnz2blQ=",
+      "url": "decode.min.js"
+    },
+    {
+      "hash": "sha256-o1qrUazFD8MPygFt+oWr3C+bSJuECWGcOxNQzps7NEk=",
+      "url": "brotliloader.min.js"
     }
   ],
   "version": "1ik0w3fh"

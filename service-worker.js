fbe779315fad81dcc7b1ac12a32bc54079abad86
Nazar62 <624rsr@gmail.com>
2025-06-20 16:26:13 +0300

    Added auto summary and markdown support2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: 0cvnlxGd */
+/* Manifest version: YiCxTWxh */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
